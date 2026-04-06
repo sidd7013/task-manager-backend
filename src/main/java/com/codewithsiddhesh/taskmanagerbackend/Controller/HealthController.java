1a792504c1bde8787b367b5e2f90de7d281e7094
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-06 07:27:02 +0530

    add health endpoint

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/Controller/HealthController.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/Controller/HealthController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
-    public String health() {
-        return "OK";
+    @GetMapping("/")
+    public String home() {
+        return "App is running 🚀";
     }
 
 }
