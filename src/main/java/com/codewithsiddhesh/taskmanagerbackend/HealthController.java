be282c1cf152242ba4172274d09a4d38150f2f06
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-06 07:55:21 +0530

    optimize startup time

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/HealthController.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/HealthController.java
@@ -1,4 +1,4 @@
-package com.codewithsiddhesh.taskmanagerbackend.Controller;
+package com.codewithsiddhesh.taskmanagerbackend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
