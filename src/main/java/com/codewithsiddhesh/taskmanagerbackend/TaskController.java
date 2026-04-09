9dae74afcc4102923e1519bcd991818306a72184
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-09 12:48:28 +0530

    fix cors

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskController.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskController.java
@@ -2,6 +2,7 @@ package com.codewithsiddhesh.taskmanagerbackend;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*")
 @RequestMapping("/tasks")
 @RestController
 public class TaskController {
