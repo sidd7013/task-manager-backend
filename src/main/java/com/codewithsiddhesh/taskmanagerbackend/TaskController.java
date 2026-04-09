46b83a0a3c67796acbbf22c3c5d35acd1e20ca44
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-09 12:08:41 +0530

    added put and delete mapping

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskController.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskController.java
@@ -2,8 +2,11 @@ package com.codewithsiddhesh.taskmanagerbackend;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +31,18 @@ public class TaskController {
 		return repo.save(task);
 	}
 	
+	@PutMapping("/{id}")
+    public Task update(@PathVariable Long id, @RequestBody Task task) {
+		Task existing = repo.findById(id).orElseThrow();
+		existing.setTitle(task.getTitle());
+		existing.setCompleted(task.isCompleted());
+		return repo.save(existing);
+   }
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repo.deleteById(id);
+	}
+
+
 }
