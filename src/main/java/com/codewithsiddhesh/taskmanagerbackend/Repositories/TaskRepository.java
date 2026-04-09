1e239b3d8ae311280bdc79ba01aad3d3c8f6988c
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-09 08:07:58 +0530

    remove db fully

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/Repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codewithsiddhesh.taskmanagerbackend.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.codewithsiddhesh.taskmanagerbackend.Entities.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long>{
-}
