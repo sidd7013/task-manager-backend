2828d3c03fbd1d588158ca29d90cecf6bd0537b5
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-11 02:04:33 +0530

    final cors filter fix

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/CorsConfig.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/CorsConfig.java
@@ -1,17 +1,28 @@
 package com.codewithsiddhesh.taskmanagerbackend;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
+public class CorsConfig {
 
-     @Override
-     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
-   }
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+
+        config.addAllowedOriginPattern("*"); // ✅ IMPORTANT
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
 }
