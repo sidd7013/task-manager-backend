1e0a3ba48fbc18eb24d968cd8c3bc694a13bbf1e
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-11 02:57:11 +0530

    fix-cors

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskController.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskController.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@RequestMapping("/tasks")
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 	
 	@Autowired
