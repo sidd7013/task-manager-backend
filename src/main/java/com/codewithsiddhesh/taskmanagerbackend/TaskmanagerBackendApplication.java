f771c269c6a29050beb9595bf4cf17cf2b53cd04
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-14 12:05:50 +0530

    fix cors globally

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskmanagerBackendApplication.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskmanagerBackendApplication.java
@@ -2,6 +2,9 @@ package com.codewithsiddhesh.taskmanagerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class TaskmanagerBackendApplication {
@@ -10,4 +13,18 @@ public class TaskmanagerBackendApplication {
 		SpringApplication.run(TaskmanagerBackendApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
 }
+
