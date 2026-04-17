deebe9ebfe4c408991254d8ff28e806df3e0b81f
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-17 07:02:10 +0530

    fix cors configuration 17-04

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskmanagerBackendApplication.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/TaskmanagerBackendApplication.java
@@ -2,9 +2,7 @@ package com.codewithsiddhesh.taskmanagerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 
 @SpringBootApplication
 public class TaskmanagerBackendApplication {
@@ -12,19 +10,6 @@ public class TaskmanagerBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerBackendApplication.class, args);
 	}
-
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
-
+	
 }
 
