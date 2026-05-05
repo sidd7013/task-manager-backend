5f14efe48b596b75fa2f313a8f7464d085eaf142
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-05-05 15:51:59 +0530

    Final working code before move to railway db

--- src/main/java/com/codewithsiddhesh/taskmanagerbackend/Task.java
+++ src/main/java/com/codewithsiddhesh/taskmanagerbackend/Task.java
@@ -1,6 +1,5 @@
 package com.codewithsiddhesh.taskmanagerbackend;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,7 +13,7 @@ public class Task {
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 private Long id;
 	 
-	 @Column(name = "text")
+
      private String title;
      
      private boolean completed;
